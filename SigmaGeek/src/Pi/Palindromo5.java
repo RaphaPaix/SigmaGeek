84d5733859d0dc45004a6afb31ab1855ca90cd1b
Rapha <Rapha@PcdoRapha>
2022-09-15 22:57:10 -0300

    Sistema parece completo

--- SigmaGeek/src/Pi/Palindromo5.java
+++ SigmaGeek/src/Pi/Palindromo5.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public class Palindromo5 {
 	private static final List<Integer> primos = new ArrayList<Integer>();
-	private static final List<Integer> zeronoinicioenofinal = new ArrayList<Integer>();
+	private static final List<Integer> palindromosprimos = new ArrayList<Integer>();
 	public static void main(String[] args) {
 		Pi pi = new Pi();
-		int n=100;
+		int n=1000;
 		pi.pi(n);
 		BigDecimal bigDecimal = pi.getValor();
 		System.out.println(bigDecimal);
@@ -56,22 +56,79 @@ public class Palindromo5 {
 				break;
 			}
 	    }
-	    //agora vamos encontrar numeros primos de 9 em 9 e adicionar em uma lista
+	    //agora vamos encontrar numeros primos em grupos de 9 em 9 e adicionar em uma lista
 	    System.out.println(dPi.length);
+	    //criar um vetor de posiçao para auxiliar
+	    int p=0;
+	    int[] posicao = new int[dPi.length];
 	    for(int i=0;i<(dPi.length-8);i++) { //pode ser que o primeiro numero seja 0, 
 	    	//mas todo numero terminado em zero é par, entao nao impora
-	 
-	    	int numeroTeste = (100000000*dPi[i])+(10000000*dPi[i+1])+(1000000*dPi[i+2])+
-	    			(100000*dPi[i+3])+(10000*dPi[i+4])+(1000*dPi[i+5])+
-	    			(100*dPi[i+6])+(10*dPi[i+7])+(1*dPi[i+8]);
-	    	System.out.println(numeroTeste);
-	    	if(ehPrimo(numeroTeste)) {
-	    		primos.add(numeroTeste);
+	    	//todo numero terminado em par é divisivel por 2
+	    	//se começa ou termina em par, nao pode ser palindromo primo
+	    	if(dPi[i]%2==0||dPi[i+8]%2==0) {
+	    		continue;
+	    	}else {
+		    	int numeroTeste = (100000000*dPi[i])+(10000000*dPi[i+1])+(1000000*dPi[i+2])+
+		    			(100000*dPi[i+3])+(10000*dPi[i+4])+(1000*dPi[i+5])+
+		    			(100*dPi[i+6])+(10*dPi[i+7])+(1*dPi[i+8]);
+		    	//System.out.println(numeroTeste);
+		    	if(ehPrimo(numeroTeste)) {
+		    		primos.add(numeroTeste);
+		    		posicao[p]=i;
+		    		p++;
+		    	}
 	    	}
 	    }
-	    System.out.println(primos);
-	    
-	    //agora vamos encontrar numeros palindromos
+	    //imprimindo numeros primos e suas posiçoes:
+	    for (int i=0;i<primos.size();i++) {
+	    	int primo = primos.get(i);
+			System.out.println("Numero: "+primo+"de posicao: "+posicao[i]);
+		}
+	    //agora vamos encontrar numeros palindromos dentro do conjunto de primos
+	    int ppp=0;
+	    int[] posicaopalindromoprimo = new int[primos.size()];
+	    for (int i=0;i<primos.size();i++) {
+	    	int primo = primos.get(i);
+	    	//primeiro transformamos o inteiro primo em um vetor de 9 espaços
+		    int[] vetorprimo = new int[9];
+			//para ser palindromo, vamos inverter e conferir se fica igual
+	    		vetorprimo[0]=primo/100000000;
+	    		vetorprimo[1]=primo/10000000;
+	    		vetorprimo[2]=primo/1000000;
+	    		vetorprimo[3]=primo/100000;
+	    		vetorprimo[4]=primo/10000;
+	    		vetorprimo[5]=primo/1000;
+	    		vetorprimo[6]=primo/100;
+	    		vetorprimo[7]=primo/10;
+	    		vetorprimo[8]=primo/1;
+	    		if(vetorprimo[0]==vetorprimo[8]) {
+	    			if(vetorprimo[1]==vetorprimo[7]) {
+	    				if(vetorprimo[2]==vetorprimo[6]) {
+	    					if(vetorprimo[3]==vetorprimo[5]) {
+	    						palindromosprimos.add(primo);
+	    						posicaopalindromoprimo[ppp]=i;
+	    						ppp++;
+	    					}
+	    				}else {
+	    					continue;
+	    				}
+	    			}else {
+	    				continue;
+	    			}
+	    		}else {
+	    			continue;
+	    		}
+		}
+	    //imprimindo lista do palindromo5 e sua posicao
+	    if(palindromosprimos.isEmpty()) {
+	    	System.out.println("Palindromo não encontrado");
+	    }else {
+	    	for(int i=0;i<palindromosprimos.size();i++) {
+	    		int palindromo = palindromosprimos.get(i);
+	    		int pos = posicaopalindromoprimo[i];
+	    		System.out.println("Numero: "+palindromo+" de posicao: "+pos);
+	    	}
+	    }
 	}
 	private static boolean ehPrimo(int numero) {
 		if(numero==0||numero==1) {
